d3b4453c45ed6429bd190b58f681ab767074ba77
simonmarcos <marcoseduardosimon@gmail.com>
2024-02-14 15:41:03 -0300

    Refactor: Updated application.properties

--- src/main/java/com/mycoachfit/api/infrastructure/config/PersistenceConfig.java
+++ src/main/java/com/mycoachfit/api/infrastructure/config/PersistenceConfig.java
@@ -1,10 +1,14 @@
 package com.mycoachfit.api.infrastructure.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EntityScan(basePackages = "com.mycoachfit.api.infrastructure.adapter.entity")
+@EnableJpaRepositories("com.mycoachfit.api.infrastructure.adapter.repository")
 class PersistenceConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
